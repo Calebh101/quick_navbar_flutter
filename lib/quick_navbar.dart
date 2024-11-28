dfe80e82e59e45a2ba68e865802be778afd6e0b2
= <=>
2024-11-28 13:11:34 -0600

    created QuickNavBar

--- lib/quick_navbar.dart
+++ lib/quick_navbar.dart
@@ -1,7 +1,48 @@
 library quick_navbar;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class QuickNavBar extends StatefulWidget {
+  final List items;
+  final Color selectedColor;
+
+  const QuickNavBar({
+    super.key,
+    required this.items,
+    this.selectedColor = Colors.blue,
+  });
+
+  @override
+  _QuickNavBarState createState() => _QuickNavBarState();
+}
+
+class _QuickNavBarState extends State<QuickNavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: widget.items[_selectedIndex]["widget"],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: widget.items.map((tab) {
+          return BottomNavigationBarItem(
+            icon: Icon(tab['icon']),
+            activeIcon: Icon(tab.containsKey('selectedIcon') ? tab["selectedIcon"] : tab["icon"]),
+            label: tab['label'],
+          );
+        }).toList(),
+        selectedItemColor: widget.selectedColor,
+      ),
+    );
+  }
 }
